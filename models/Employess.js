e2bdb90e74c4809e1ffa4905892fd7951feb8863
Sanskar-9264 <sanskarg9264@gmail.com>
2026-01-11 22:35:47 +0530

    Add Employee model with schema definition

--- models/Employess.js
+++ models/Employess.js
@@ -1,1 +1,14 @@
 
+const { default: mongoose } = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+    name:String,
+    salary:Number,
+    Language:String,
+    city:String,
+    isManager: Boolean
+})
+
+const Employee = mongoose.model('Employee' ,employeeSchema)
+module.exports = Employee
+
